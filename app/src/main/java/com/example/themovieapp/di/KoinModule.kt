9ba5ca07e40f919753aba761463e4397de18ee4e
Norman Sánchez <normansanchezn@gmail.com>
2024-11-07 20:02:58 -0600

    [⚙️] Koin configuration

---
+++ app/src/main/java/com/example/themovieapp/di/KoinModule.kt
@@ -0,0 +1,7 @@
+package com.example.themovieapp.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
--- app/src/main/java/com/example/themovieapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/themovieapp/presentation/ui/MainActivity.kt
@@ -1,10 +1,9 @@
-package com.example.themovieapp
+package com.example.themovieapp.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.themovieapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
