c28db6b00480d0849aeb264ce5b5eabc7857d4c9
Norman Sánchez <normansanchezn@gmail.com>
2024-11-11 16:08:41 -0600

    [🐛] Fix Recyclerview

--- app/src/main/java/com/example/themovieapp/presentation/ui/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/themovieapp/presentation/ui/adapter/MovieAdapter.kt
@@ -1,36 +1,32 @@
 package com.example.themovieapp.presentation.ui.adapter
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.themovieapp.data.model.MovieEntity
-import com.example.themovieapp.databinding.ItemMovieViewBinding
+import com.example.themovieapp.presentation.customview.ItemMovieView
 
 class MovieAdapter: ListAdapter<MovieEntity, MovieAdapter.MovieViewHolder>(MovieDiffCallback()) {
 
     private var movieList = mutableListOf<MovieEntity>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val binding = ItemMovieViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MovieViewHolder(binding)
+        val itemMovieView = ItemMovieView(parent.context)
+        return MovieViewHolder(itemMovieView)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        holder.bind(movieList[position])
+        val movie = movieList[position]
+        holder.bind(movie)
     }
 
     fun setListOfMovies(movies: List<MovieEntity>) {
         movieList = movies.toMutableList()
     }
 
-    inner class MovieViewHolder(private val binding: ItemMovieViewBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class MovieViewHolder(private val itemMovieView: ItemMovieView) : RecyclerView.ViewHolder(itemMovieView) {
         fun bind(movie: MovieEntity) {
-            binding.movieTitle.text = movie.title
-            Glide.with(binding.root.context)
-                .load("https://image.tmdb.org/t/p/w500/${movie.posterPath}")
-                .into(binding.movieImage)
+            itemMovieView.bind(movie.title, movie.posterPath)
         }
     }
 }
